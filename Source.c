433d27070af829211c4291807c9c32b0c5e4af65
DestructiveReasoning <harleyw@hotmail.com>
2015-07-13 22:29:29 +0000

    First update, better graphics

--- Source.c
+++ Source.c
@@ -65,17 +65,18 @@ WINDOW *game = NULL;
 
 void drawCell(int x,int y);
 void eraseCell(int x, int y);
-void plotCells(int cells[]);
 int checkNeighbors(int x, int y, int cells[]);
 void update(int cells[], int size);
 void updateWin(int cells[], int size);
+
+void initCurses();
+void showSplash();
 
 void render(int cells[], int size)
 {
 	int c;
 	for(c = 0; c < size; c++)
 	{
-//		cells[c] == 0 ? eraseCell(c % ys, c / ys) : drawCell(c % ys, c / ys);
 		if(cells[c] == 0) eraseCell(c % ys, c / ys);
 		else drawCell(c % ys, c / ys);
 	}
@@ -86,50 +87,43 @@ void destroyWindow(WINDOW *win);
 
 int main(int argc, char *argv[])
 {
+	if(argc >= 2)
+	{
+		if(strcmp(argv[1],"-h") == 0 || strcmp(argv[1],"--help") == 0)
+		{
+			printf("\nConway\n");
+			printf("Copyright 2015 Harley Wiltzer\n\n");
+			printf("conway:\t\tOpen conway with a random color\n");
+			printf("conway 1:\tOpen conway blue theme\n");
+			printf("conway 2:\tOpen conway red theme\n");
+			printf("conway 3:\tOpen conway green theme\n");
+			printf("conway 4:\tOpen conway yellow theme\n");
+			printf("conway 5:\tOpen conway light blue theme\n");
+			printf("conway 6:\tOpen conway orange theme\n");
+			printf("conway 7:\tOpen conway white theme\n");
+			printf("\n");
+			printf("While running conway...\n");
+			printf("=======================\n");
+			printf("q:\t\tClose conway\n");
+			printf("space:\t\tActivate/Kill cell\n");
+			printf("enter:\t\tStart ticking\n");
+			printf("h:\t\tMove cursor left\n");
+			printf("j:\t\tMove cursor down\n");
+			printf("k:\t\tMove cursor up\n");
+			printf("l:\t\tMove cursor right\n");
+			printf("d:\t\tClear all cells\n");
+			printf("=:\t\tIncrease the amount of ticks by 10\n");
+			printf("-:\t\tDecrease the amount of ticks by 10\n");
+			printf("+:\t\tIncrease the amount of ticks by 1\n");
+			printf("_:\t\tDecrease the amount of ticks by 1\n");
+			return 0;
+		}
+	}
 	int i, c;
-	initscr();
-	start_color();
-	init_color(COLOR_MAGENTA,266,266,465);
-	init_color(COLOR_RED,1000,156,156);
-	init_color(COLOR_GREEN,332,730,332);
-	init_color(COLOR_YELLOW,742,742,0);
-	init_color(COLOR_BLUE,500,500,1000);
-	init_color(COLOR_CYAN,1000,500,0);
-	init_pair(1,COLOR_MAGENTA,COLOR_BLACK);
-	init_pair(2,COLOR_RED,COLOR_BLACK);
-	init_pair(3,COLOR_GREEN,COLOR_BLACK);
-	init_pair(4,COLOR_YELLOW,COLOR_BLACK);
-	init_pair(5,COLOR_BLUE,COLOR_BLACK);
-	init_pair(6,COLOR_CYAN,COLOR_BLACK);
-	init_pair(7,COLOR_WHITE,COLOR_BLACK);
-
-	raw(); 			//Disable line buffering
-	keypad(stdscr, TRUE);	//Get access to all keys
-	cbreak();
-	noecho();		//No 'echo' on wgetch(game)
-	getmaxyx(stdscr,rows,cols);
-
-	attron(COLOR_PAIR(YELLOW)|A_BOLD);
-	mvprintw(0,0,"+");
-	mvprintw(rows -1, 0, "+");
-	mvprintw(0,cols - 1, "+");
-	mvprintw(rows - 1, cols - 1, "+");
-	for(i = 1; i < cols - 1; i++)
-	{
-		mvprintw(0,i,"-");
-		mvprintw(rows - 1, i, "-");
-	}
-	for(i = 1; i < rows - 1; i++)
-	{
-		mvprintw(i,0,"|");
-		mvprintw(i,cols - 1, "|");
-	}
-
-	mvprintw(rows/2 - 1,cols/2 - 3, "CONWAY");
-	attroff(A_BOLD);
-	mvprintw(rows/2 + 1,cols/2 - 16, "Powered by Destructive Reasoning");
-	attroff(COLOR_PAIR(YELLOW));
-
+	
+	initCurses();
+	showSplash();
+	
 	refresh();
 	getch();
 
@@ -166,7 +160,6 @@ int main(int argc, char *argv[])
 		{
 			color = atoi(argv[1]);
 		}
-
 	}
 
 	x = ys/2;
@@ -259,11 +252,9 @@ int main(int argc, char *argv[])
 		updateWin(cells,area/2);
 		wrefresh(game);
 	}
-	int gameIsNull = (win == NULL);
 	destroyWindow(game);
 	destroyWindow(win);
 	endwin();
-	printf("Game: %d\n",gameIsNull);
 	return 0;
 }
 
@@ -271,8 +262,8 @@ WINDOW *createWindow(int width, int height, int starty, int startx)
 {
 	WINDOW *local_win;
 	local_win = newwin(height,width,starty,startx);
-	wmove(local_win,1,1);
-	x = y = 1;
+//	wmove(local_win,1,1);
+//	x = y = 1;
 	wrefresh(local_win);
 	return local_win;
 }
@@ -300,21 +291,6 @@ void eraseCell(int x, int y)
 	mvwprintw(game,y, x * 2 + 1, " ");
 	wattroff(game,COLOR_PAIR(color));
 	wmove(game,y,x * 2);
-}
-
-void plotCells(int cells[])
-{
-	int c, r;
-	for(c = 0; c < ys; c++)
-	{
-		for(r = 0; r < xs; r++)
-		{
-			if(cells[r * ys + c] == 1)
-			{
-				drawCell(c,r);
-			}
-		}
-	}
 }
 
 int checkNeighbors(int x, int y, int cells[])
@@ -394,7 +370,6 @@ void update(int cells[], int size)
 			}
 		}
 		updateWin(cells,size);
-//		refresh();
 		wrefresh(game);
 		usleep(SLEEP_TIME);
 		ticks--;
@@ -417,6 +392,76 @@ void updateWin(int cells[], int size)
 	mvwprintw(win,rows/2 - 2,2,"Board Size: [%d, %d]",xs,ys);
 	mvwprintw(win,rows/2 - 1,2,"Cells Alive: %d",cellsAlive);
 	mvwprintw(win,rows/2,2,"Ticks Remaining: %d",ticks);
-//	refresh();
 	wrefresh(win);
+}
+
+void initCurses()
+{
+	initscr();
+	start_color();
+	init_color(COLOR_MAGENTA,266,266,465);
+	init_color(COLOR_RED,1000,156,156);
+	init_color(COLOR_GREEN,332,730,332);
+	init_color(COLOR_YELLOW,742,742,0);
+	init_color(COLOR_BLUE,500,500,1000);
+	init_color(COLOR_CYAN,1000,500,0);
+	init_pair(1,COLOR_MAGENTA,COLOR_BLACK);
+	init_pair(2,COLOR_RED,COLOR_BLACK);
+	init_pair(3,COLOR_GREEN,COLOR_BLACK);
+	init_pair(4,COLOR_YELLOW,COLOR_BLACK);
+	init_pair(5,COLOR_BLUE,COLOR_BLACK);
+	init_pair(6,COLOR_CYAN,COLOR_BLACK);
+	init_pair(7,COLOR_WHITE,COLOR_BLACK);
+
+	raw(); 			//Disable line buffering
+	keypad(stdscr, TRUE);	//Get access to all keys
+	cbreak();
+	noecho();		//No 'echo' on wgetch(game)
+	getmaxyx(stdscr,rows,cols);
+}
+
+void showSplash()
+{
+	int i;
+	char logoPath[256];
+	FILE *fp;
+	strcat(logoPath,getenv("HOME"));
+	strcat(logoPath,"/.conway/conwaylogo\0");
+//	char *logoPath = "/home/harwiltz/.conway/conwaylogo";
+	fp = fopen(logoPath,"r");
+	char *line = NULL;
+	size_t length = 0;
+	ssize_t read;
+	int curcol = rows/2 - 10;
+	int size = 0;
+
+	while((read = getline(&line,&length,fp)) != -1)
+	{
+		char *stroke = line;
+		if(size == 0) for(size = 0; stroke[size] != '\0' && stroke[size] != '\n'; size++);
+		mvprintw(curcol++,cols/2 - size/2,stroke);
+		line = NULL;
+	}
+	fclose(fp);
+
+	attron(COLOR_PAIR(YELLOW)|A_BOLD);
+	mvprintw(0,0,"+");
+	mvprintw(rows -1, 0, "+");
+	mvprintw(0,cols - 1, "+");
+	mvprintw(rows - 1, cols - 1, "+");
+	for(i = 1; i < cols - 1; i++)
+	{
+		mvprintw(0,i,"-");
+		mvprintw(rows - 1, i, "-");
+	}
+	for(i = 1; i < rows - 1; i++)
+	{
+		mvprintw(i,0,"|");
+		mvprintw(i,cols - 1, "|");
+	}
+
+	mvprintw(curcol + 1,cols/2 - 3, "CONWAY");
+	attroff(A_BOLD);
+	mvprintw(curcol + 2,cols/2 - 16, "Powered by Destructive Reasoning");
+	attroff(COLOR_PAIR(YELLOW));
 }
